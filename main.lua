9dbb864983245dbfc92a507f8f8b145fbada3522
Kafelekk <kafelekk2000@gmail.com>
2023-03-12 14:48:44 +0100

    code optimization

--- main.lua
+++ main.lua
@@ -4,53 +4,26 @@ local level = game:GetLevel()
 local player = Isaac.GetPlayer(0)
 
 local actiroom = nil
-local cursecon = 69 -- this just stores current floor starting curses, or deafault 69 so i dont need to care about one small bug which ceased to exist after assiging 69 as deafault value
+local cursecon = 69 -- stores current floor starting curses, or deafault 69(curse of blind, curse of the lost, curse of darkness)
+
 
 --Costume ids
 local GLITCHED_CROWN = Isaac.GetCostumeIdByPath("gfx/characters/GlitchedCrown.anm2")
 local BANDAGE = Isaac.GetCostumeIdByPath("gfx/characters/BandageClear.anm2")
 
---Curses that king get on every floor
-local Curses = {
-	CURSE_OFD = 1,
-	CURSE_OFTL = 4,
-	CURSE_OFB = 64,
-}
-
+local gcCostumeEquipped = false
 --Get item ids
-local IteemId = {
+local ItemId = {
 	GLITCHEDCROWN = Isaac.GetItemIdByName("Glitched Crown"),
 	BLACKMATCHBOX = Isaac.GetItemIdByName("Black Match Box"),
 }
 
---stuff
-local Hasthisiteem = {
-	Glitchedcrown = false,
-	Blackmatchbox = false,
-}
-
-local function UpdateIteems(player)
-	Hasthisiteem.Glitchedcrown = player:HasCollectible(IteemId.GLITCHEDCROWN)
-	Hasthisiteem.Blackmatchbox = player:HasCollectible(IteemId.BLACKMATCHBOX)
-end
-
-function BKing:onPlayerInit(player)
-	UpdateIteems(player)
-end
-
-BKing:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, BKing.onPlayerInit)
-
--- adds bandage costume (i dont really remember why i wanted this as costume instead of puting it on character sprite, i guess i should at least note some of my ideas)
+-- adds bandage costume
 function BKing:OnINIT(player)
 	if player:GetName() == "BlindKing" then
-		iscursed = true -- default true 
-	end
-	local costumeEquipped = false
-	if player:GetName() == "BlindKing" then
+		local curse = level.GetCurses(level)
+		local iscursed = true -- default true
 		player:AddNullCostume(BANDAGE)
-		costumeEquipped = true
-	else
-		costumeEquipped = false
 	end
 end
 BKing:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, BKing.OnINIT)
@@ -59,45 +32,33 @@ BKing:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, BKing.OnINIT)
 function BKing:PostUpdate()
 	local player = Isaac.GetPlayer(0)
 	local level = game:GetLevel()
-	local curse = level.GetCurses(level)
-	-- checks if player is cursed
-	if player:GetName() == "BlindKing" then
-		if curse < cursecon then
-			iscursed = false
-		elseif curse >= cursecon then
-			iscursed = true
-		end
-	end
-	-- add this REALLY COOL looking glitched crown costume (I will make this costume look better in future TwT ... (I will probably forget about this "trival" matter) )
-	if player:HasCollectible(689) then
-		if Hasthisiteem.Glitchedcrown ~= true then
+	-- adds glitched crown costume 
+	if player:HasCollectible(689) then 
+		if gcCostumeEquipped ~= true then
 			player:AddNullCostume(GLITCHED_CROWN)
-			Hasthisiteem.Glitchedcrown = true
+			gcCostumeEquipped = true
 		end
-	elseif Hasthisiteem.Glitchedcrown == true and player:HasCollectible(689) == false then
+	elseif gcCostumeEquipped == true and player:HasCollectible(689) == false then
 		player:TryRemoveNullCostume(GLITCHED_CROWN)
-		Hasthisiteem.Glitchedcrown = false
+		gcCostumeEquipped = false
 	end
 end
 BKing:AddCallback(ModCallbacks.MC_POST_UPDATE, BKing.PostUpdate)
 
---Great thanks to the guy from reddit modding community who helped me with solving error (i was dumb back then, not that something change but... yeah)
-function Curses:onEval(CurseFlags) 
+function BKing:onEval(CurseFlags) 
 	local player = Isaac.GetPlayer(0)
 	if player:GetName() == "BlindKing" then
-		CurseFlags = CurseFlags | Curses.CURSE_OFD
-		CurseFlags = CurseFlags | Curses.CURSE_OFB
-		CurseFlags = CurseFlags | Curses.CURSE_OFTL
+		CurseFlags = CurseFlags | cursecon
 		cursecon = CurseFlags
+		iscursed = true
 		return CurseFlags
 	end
 	return CurseFlags
 end
+ 
+BKing:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, BKing.onEval)
 
-BKing:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, Curses.onEval)
-
---stolen from yt tutorial
-local statz = { --relative to isaac stats
+local stats = { --relative to isaac stats
 	DAMAGE = 1.5,
 	SPEED =  0,
 	SHOTSPEED = 0,
@@ -107,79 +68,35 @@ local statz = { --relative to isaac stats
 	TEARFLAG = 0,
 	TEARCOLOR = Color(1.0, 1.0, 0.3, 0.7, 0, 0, 0),
 }
-local abyssgaze = false
-function BKing:onCache( player, cacheFlag)
-	if player:GetName() == "BlindKing" then
-		if cacheFlag == CacheFlag.CACHE_DAMAGE then
-			player.Damage = player.Damage + statz.DAMAGE
-			if abyssgaze == true then
-				if debuffcheck.DMGcheck == true then
-					player.Damage = player.Damage - (player.Damage * debufflist.DMGdebuff)
-				end
-			end
-		end
-		if cacheFlag == CacheFlag.CACHE_SHOTSPEED then
-			player.ShotSpeed = player.ShotSpeed + statz.SHOTSPEED
-		end
-		if cacheFlag == CacheFlag.CACHE_RANGE then
-			player.TearRange = player.TearRange + statz.TEARRANGE
-		end
-		if cacheFlag == CacheFlag.CACHE_SPEED then
-			player.MoveSpeed = player.MoveSpeed + statz.SPEED
-			if abyssgaze == true then
-				if debuffcheck.SPEEDcheck == true then
-					player.MoveSpeed = player.MoveSpeed - (player.MoveSpeed * debufflist.SPEEDdebuff)
-				end
-			end
-		end
-		if cacheFlag == CacheFlag.CACHE_LUCK then
-			player.Luck = player.Luck + statz.LUCK
-		end
-		if cacheFlag == CacheFlag.CACHE_FLYING and statz.FLYING then
-			player.CanFly = true
-		end
-		if cacheFlag == CacheFlag.CACHE_TEARFLAG then
-			player.TearFlags = player.TearFlags | statz.TEARFLAG
-		end
-		if cacheFlag == CacheFlag.CACHE_TEARCOLOR then
-			player.TearColor = statz.TEARCOLOR
-		end
-	end
-end
 
-BKing:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, BKing.onCache)
---
-
--- Adds the best active item
--- not really
 function BKing:onUpdate(player)
 	if game:GetFrameCount() == 1 then
 		if player:GetName() == "BlindKing" then
-			player:AddCollectible(IteemId.BLACKMATCHBOX, 8, true, ActiveSlot.SLOT_PRIMARY)
+			player:AddCollectible(ItemId.BLACKMATCHBOX, 8, true, ActiveSlot.SLOT_PRIMARY)
 		end
 	end
 end
 
 BKing:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, BKing.onUpdate)
 
-	--BlackMatchBox active effect
+--BlackMatchBox active effect
 function BKing:ActivateBlackMatchBox (_type, player)
 	local curse = level.GetCurses(level)
 	local player = Isaac.GetPlayer(0)
-	actiroom = level:GetCurrentRoomIndex()-- checks for room index in which the item was used
+	activationroom = level:GetCurrentRoomIndex()-- checks for room index in which the item was used
 	iscursed = false
 	level:RemoveCurses(curse)
 	player:TakeDamage(2, DamageFlag.DAMAGE_NO_PENALTIES, EntityRef(player), 0)
 end
-BKing:AddCallback(ModCallbacks.MC_USE_ITEM, BKing.ActivateBlackMatchBox, IteemId.BLACKMATCHBOX)
+BKing:AddCallback(ModCallbacks.MC_USE_ITEM, BKing.ActivateBlackMatchBox, ItemId.BLACKMATCHBOX)
 
 function BKing:AgainCursed()
-	local currentroom = level:GetCurrentRoomIndex() -- checks for current room index so King can be cursed again
+	local currentroom = level:GetCurrentRoomIndex() -- checks for current room index
 	if iscursed == false then
-		if currentroom ~= actiroom then
+		if currentroom ~= activationroom then
 			level:AddCurse(cursecon, false)
 		end
 	end
 end
-BKing:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, BKing.AgainCursed)
+BKing:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, BKing.AgainCursed)
 --
